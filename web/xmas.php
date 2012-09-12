e491f94f6662de230b2afeebf6c9e388cea0b17e
U-Zyn Chua <chua@uzyn.com>
2012-09-12 09:49:31 +0000

    Some files

---
+++ web/xmas.php
@@ -0,0 +1,29 @@
+#!/usr/bin/php
+<?php
+require_once(dirname(dirname(__FILE__)).'/PHP-GPIO/GPIO.php');
+
+$leds = array(22, 17, 18);
+
+$gpio = new GPIO();
+
+foreach ($leds as $led) {
+	$gpio->setup($led, "out");
+}
+
+$i = 0;
+while(true) {
+	foreach ($leds as $j=>$led) {
+		if ($i==$j) {
+			$gpio->output($led, 1);
+		}
+		else {
+			$gpio->output($led, 0);
+		}
+	}
+
+	++$i;
+echo $i;
+	if ($i >= 3) $i = 0;
+	usleep(0.5*1000000);
+}
+
